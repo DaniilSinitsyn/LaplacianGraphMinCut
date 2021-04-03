c490859dfbc5ca946574b10c1e315d3d854d785b
D.Sinitsyn <dansinitsyn@gmail.com>
2021-04-04 00:00:57 +0300

    temp

---
+++ graph.hpp
@@ -0,0 +1,182 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <iostream>
+#include <random>
+#include <ranges>
+#include <stack>
+#include <vector>
+
+#include <Eigen/Dense>
+#include <Eigen/Eigen>
+#include <Eigen/SPQRSupport>
+
+struct Vert {
+  int n;
+};
+
+struct Edge {
+  int v1, v2;
+  float cap;
+};
+
+struct Graph {
+  Graph(const int ver_n) : verts_number(ver_n + 2), maxflow_upper_limit(0.0) {
+    potentials.resize(verts_number);
+    b = Eigen::VectorXf::Zero(ver_n + 2);
+    b(0) = 1;
+    b(verts_number - 1) = -1;
+    source_sum = 0;
+    terminal_sum = 0;
+  }
+
+  void AddNode(int n, float source, float terminal) {
+    if (abs(source) > 1e-8) {
+      edges.emplace_back(0, n + 1, source);
+      source_sum += source;
+    }
+    if (abs(terminal) > 1e-8)
+      edges.emplace_back(n + 1, verts_number - 1, terminal);
+    terminal_sum += terminal;
+  }
+
+  void AddEdge(int n1, int n2, float capacity, float reverseCapacity) {
+    float w = (capacity + reverseCapacity);
+    if (abs(w) > 1e-8)
+      edges.emplace_back(n1 + 1, n2 + 1, w);
+  }
+
+  float ComputeMaxFlow() {
+    maxflow_upper_limit = std::min(source_sum, terminal_sum);
+
+    rho = 3 * std::pow(edges.size(), 1.0 / 3.0) * std::pow(eps, -2.0 / 3.0);
+
+    float min_dist = maxflow_upper_limit * 0.01;
+    std::pair<float, float> flow_pair(0, maxflow_upper_limit);
+    float good;
+    while (flow_pair.second - flow_pair.first > min_dist) {
+      float center = (flow_pair.first + flow_pair.second) / 2.0;
+      float f = knownFlowCut(center);
+      std::cout << flow_pair.first << ' ' << flow_pair.second << " - " << f
+                << "(" << center << ")" << '\n';
+      return f;
+      if (f >= 0) {
+        good = center;
+        flow_pair.second = center;
+      } else {
+        break;
+      }
+    }
+    std::cout << cut_thresh << std::endl;
+    return knownFlowCut(good);
+  }
+  inline bool IsNodeOnSrcSide(int n) const {
+    return potentials(n) >= cut_thresh - 0.05;
+  }
+
+private:
+  float knownFlowCut(float maxFlow) {
+    weights.resize(edges.size());
+    weights.setOnes();
+    size_t N = 5 * std::pow(eps, -8.0 / 3.0) *
+               std::pow(edges.size(), 1.0 / 3.0) * log(edges.size());
+
+    for (size_t i = 0; i < N; ++i) {
+      solveSparse(maxFlow);
+      float mu = weights.sum();
+      Eigen::VectorXf prev_w = weights;
+      for (int j = 0; j < weights.size(); ++j) {
+        weights(j) =
+            weights(j) * (1 + eps / rho * abs(flow(j)) / edges[j].cap) +
+            mu * eps * eps / edges.size() / rho;
+      }
+      std::cout << i << " " << (prev_w - weights).norm() << std::endl;
+
+      potentials = potentials.array() - potentials(verts_number - 1);
+      potentials /= potentials(0);
+
+      float min_cut_capacity = std::numeric_limits<float>::max();
+      float potential_max = potentials.maxCoeff();
+      float good_cap = 0;
+      std::cout << "Pot max : " << potential_max << std::endl;
+      for (int k = 1; k < 40; ++k) {
+        float a = potential_max * float(k) / 40;
+        float curr_cut = 0;
+        float cap = 0;
+        for (size_t k = 0; k < edges.size(); ++k) {
+          auto &e = edges[k];
+          float max = std::max(potentials[e.v1], potentials[e.v2]);
+          float min = std::min(potentials[e.v1], potentials[e.v2]);
+          if (min < a && a < max) {
+            // curr_cut += e.cap;
+            cap += abs(e.cap);
+            if (min == potentials[e.v1])
+              curr_cut += flow(k);
+            else
+              curr_cut -= flow(k);
+          }
+        }
+        if (min_cut_capacity > curr_cut && curr_cut != 0) {
+          cut_thresh = a;
+          min_cut_capacity = curr_cut;
+          good_cap = cap;
+        }
+      }
+      std::cout << min_cut_capacity << ' ' << cut_thresh << std::endl;
+      if (min_cut_capacity < 0)
+        return -1;
+      if (good_cap < maxFlow / (1 - 7 * eps))
+        return good_cap;
+    }
+    return -1.0;
+  }
+
+  void solveSparse(float maxFlow) {
+    SpMat B(verts_number, edges.size());
+    B.reserve(Eigen::VectorXi::Constant(edges.size(), 2));
+    for (size_t i = 0; i < edges.size(); ++i) {
+      B.insert(edges[i].v1, i) = -1;
+      B.insert(edges[i].v2, i) = 1;
+    }
+    B.makeCompressed();
+
+    Eigen::VectorXf resistances(edges.size());
+    for (size_t i = 0; i < edges.size(); ++i) {
+      resistances(i) = edges[i].cap * edges[i].cap / weights(i);
+    }
+    SpMat A = B * resistances.asDiagonal() * SpMat(B.transpose());
+    A.makeCompressed();
+    Eigen::VectorXf x = Eigen::VectorXf::Zero(A.cols());
+    x(0) = 1;
+    x(1) = -1;
+    cg.compute(A);
+
+    potentials = cg.solveWithGuess(maxFlow * b, x);
+    std::cout << cg.error() << " " << cg.iterations() << std::endl;
+    flow = resistances.array() * (SpMat(B.transpose()) * potentials).array();
+
+    auto kek = (B * flow).eval();
+    std::cout << kek(0) << ' ' << kek(verts_number - 1) << ' '
+              << kek.segment(1, verts_number - 2).norm() << std::endl;
+  }
+
+  using SpMat = Eigen::SparseMatrix<float, Eigen::ColMajor>;
+  Eigen::ConjugateGradient<SpMat, Eigen::Upper | Eigen::Lower> cg;
+  std::vector<Edge> edges;
+
+  const int verts_number;
+
+  Eigen::VectorXf b;
+
+  Eigen::VectorXf weights;
+  Eigen::VectorXf flow;
+  Eigen::VectorXf potentials;
+
+  float maxflow_upper_limit;
+  float source_sum, terminal_sum;
+  float eps = 0.01;
+  float rho;
+  float cut_thresh;
+};
+
+#endif // GRAPH_HPP
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "graph.hpp"
+#include <chrono>
+#include <fstream>
+int main() {
+  // int verts = 532661, edges = 1065322;
+  int verts = 11537960, edges = 23075920;
+  Graph g(verts);
+  Eigen::setNbThreads(8);
+
+  std::ifstream file("/home/little/cscenter/practice/last_sem/datasets/"
+                     "herzjesu/openmvg/openvms/graph.txt");
+  for (int i = 0; i < verts + edges; ++i) {
+    char c;
+    file >> c;
+    if (c == 'n') {
+      int n;
+      float source, terminal;
+      file >> n >> source >> terminal;
+      g.AddNode(n, source, terminal);
+    }
+    if (c == 'e') {
+      int n1, n2;
+      float cap, revCap;
+      file >> n1 >> n2 >> cap >> revCap;
+      g.AddEdge(n1, n2, cap, revCap);
+    }
+  }
+  std::cout << "Solution start" << std::endl;
+  auto start = std::chrono::high_resolution_clock::now();
+  g.ComputeMaxFlow();
+  auto stop = std::chrono::high_resolution_clock::now();
+  std::cout << "elapsed time : "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(stop -
+                                                                     start)
+                   .count()
+            << std::endl;
+  int src = 0, term = 0;
+  for (int v = 0; v < verts + 2; ++v) {
+    if (g.IsNodeOnSrcSide(v))
+      src++;
+    else
+      term++;
+  }
+
+  std::ofstream save("src.txt");
+  save << src << std::endl;
+  for (int v = 1; v < verts + 1; ++v) {
+    if (g.IsNodeOnSrcSide(v))
+      save << (v - 1) << std::endl;
+  }
+  std::cout << "source : " << src << ", term : " << term << std::endl;
+
+  return 0;
+}
